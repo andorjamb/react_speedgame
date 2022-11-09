31f5389154612155ba935c1cab23811a7bd1e04d
Anna Petelin <adpetelin@gmail.com>
2022-11-09 14:33:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import Circle from './Circle';
+
+class App extends Component {
+
+  state = {
+    score: 0,
+    circles: [
+      { key: 1 },
+      { key: 2 },
+      { key: 3 },
+      { key: 4 },
+    ],
+  }
+
+  scoreHandler = () => {
+    this.setState({ score: this.state.score + 1 })
+  }
+
+  render() {
+    const circles =  
+      this.state.circles.map((circle) => {
+        return (
+        <div>
+          <p>test</p>
+          <Circle key={circle.key}
+        />
+        </div>
+        )  
+      }
+      )
+      console.log(circles);
+    
+    return (
+      <div className="App">
+        <h1>Speedgame</h1>
+        <div className="score-container">
+          <p id="scoreDisplay">{this.state.score}</p></div>
+        <div className="circle-container">
+          {circles}
+        </div>
+        <div className="button-container">
+          <button id="startButton">Start Game</button>
+          <button id="stopButton">Stop Game</button>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
